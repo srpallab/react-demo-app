a09c1ff186ea48dd98c97a894b9c670e4ea75da5
Shafiqur Rahman <pallab.alif@gmail.com>
2021-09-20 23:47:48 +0600

    ADD: State, Event and Load data.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExtarnalUsers></ExtarnalUsers>
+    </div>
+  );
+}
+
+function ExtarnalUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    console.log("Inside");
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+  return (
+    <div>
+    {users.map(user => <User name={user.name} username={user.username} email={user.email}></User>)}
+    </div>
+  );
+}
+
+function User(props){
+  return (
+    <div>
+      <h1>Name: {props.name}</h1>
+      <p>Username: {props.username}</p>
+      <p>Email: {props.email}</p>
     </div>
   );
 }
